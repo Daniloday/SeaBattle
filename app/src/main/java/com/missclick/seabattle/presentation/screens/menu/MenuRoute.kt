053f5baaefbf38baea36dd34e035c7195884c09c
daniloday <danil.vladyka1@gmail.com>
2024-01-23 17:09:45 +0200

    how to navigate

--- app/src/main/java/com/missclick/seabattle/presentation/screens/menu/MenuRoute.kt
+++ app/src/main/java/com/missclick/seabattle/presentation/screens/menu/MenuRoute.kt
@@ -1,6 +1,7 @@
 package com.missclick.seabattle.presentation.screens.menu
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -12,29 +13,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.missclick.seabattle.presentation.navigation.NavigationTree
 import com.missclick.seabattle.presentation.ui.theme.SeaBattleTheme
 
 @Composable
-fun MenuRoute(vm : MenuViewModel = hiltViewModel() ){
+fun MenuRoute(navController: NavController, vm : MenuViewModel = hiltViewModel() ){
 
     val uiState by vm.uiState.collectAsState()
 
     MenuScreen(
+        uiState = uiState,
         obtainEvent = vm::obtainEvent,
-        uiState = uiState
+        navigateTo = {navController.navigate(it.route)}
     )
 
 }
 
 @Composable
 fun MenuScreen(
+    uiState: MenuUiState,
     obtainEvent : (MenuEvent) -> Unit,
-    uiState: MenuUiState
+    navigateTo : (NavigationTree) -> Unit
 ){
 
     Box(modifier = Modifier
         .size(100.dp)
-        .background(Color.Gray)){
+        .background(Color.Gray)
+        .clickable { navigateTo(NavigationTree.Menu) }){
         Text(text = uiState.a, color = Color.Red)
     }
     

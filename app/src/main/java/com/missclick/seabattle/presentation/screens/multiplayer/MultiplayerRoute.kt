4306679780407034d7d2d50053dc0bd557581275
incarnadine24 <wenqzz.123@gmail.com>
2024-01-23 19:08:39 +0200

    commit#3

--- app/src/main/java/com/missclick/seabattle/presentation/screens/multiplayer/MultiplayerRoute.kt
+++ app/src/main/java/com/missclick/seabattle/presentation/screens/multiplayer/MultiplayerRoute.kt
@@ -1,11 +1,20 @@
 package com.missclick.seabattle.presentation.screens.multiplayer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,10 +25,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.missclick.seabattle.R
+import com.missclick.seabattle.presentation.components.BackMark
 import com.missclick.seabattle.presentation.navigation.NavigationTree
 import com.missclick.seabattle.presentation.screens.menu.MenuEvent
 
@@ -50,30 +65,43 @@ fun MultiplayerScreen(
     Box(modifier = Modifier.fillMaxSize()){
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxSize(0.7F)
                 .align(Alignment.Center),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly
 
         ) {
-            TextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text(stringResource(id = R.string.label)) }
-            )
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.SpaceEvenly){
+                TextField(
+                    value = text,
+                    modifier = Modifier.clip(shape = RoundedCornerShape(10.dp)).border(1.dp, color = AppTheme.colors.primary,shape = RoundedCornerShape(10.dp)).background(AppTheme.colors.secondaryBackground).width(width = 230.dp),
+                    textStyle = AppTheme.typography.h3,
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.label),color=AppTheme.colors.secondary, style = AppTheme.typography.h3)
+                    },
+                    onValueChange = { text = it },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+
+                Image(painter = painterResource(id = R.drawable.select_mark), modifier = Modifier.padding(start = 24.dp).size(36.dp).align(Alignment.CenterVertically), contentDescription = null)
+            }
+
             Text(text = stringResource(id = R.string.or), modifier = Modifier.clickable {
 
             },
-                style = AppTheme.typography.headerTextBold,
-                color = AppTheme.colors.primary
+                style = AppTheme.typography.h2,
+                color = AppTheme.colors.secondary
             )
             Text(text = stringResource(id = R.string.create_room), modifier = Modifier.clickable {
 
             },
-                style = AppTheme.typography.headerTextBold,
+                style = AppTheme.typography.h2,
                 color = AppTheme.colors.primary
             )
-
+        }
+        BackMark(){
+            navigateTo(NavigationTree.Menu)
         }
     }
 }

5ad11305db1b164c7520961ab4d9c68feead703e
alexeosss <oleksii.melnychenko.kpi@gmail.com>
2024-01-23 17:18:43 +0200

    alexm-typography

--- app/src/main/java/com/missclick/seabattle/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/missclick/seabattle/presentation/ui/theme/Type.kt
@@ -3,21 +3,42 @@ package com.missclick.seabattle.presentation.ui.theme
 
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.missclick.seabattle.R
 
 
 data class Typography(
-    val headerTextBold: TextStyle,
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val h4: TextStyle,
 )
 
 
 val typography = Typography(
-    headerTextBold = TextStyle(
-        fontSize = 36.sp,
-        fontWeight = FontWeight.Bold
+    h1 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.rubik_bubbles)),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal
     ),
+    h2 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.rubik_bubbles)),
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    h3 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.rubik_bubbles)),
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    h4 = TextStyle(
+        fontFamily = FontFamily(Font(R.font.rubik_bubbles)),
+        fontSize = 46.sp,
+        fontWeight = FontWeight.Normal
+    )
 
 )
 

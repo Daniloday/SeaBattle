bb0ffdd0592dfe08260e84d4063f455fd136d6ba
incarnadine24 <wenqzz.123@gmail.com>
2024-01-23 18:01:58 +0200

    commit#2

--- app/src/main/java/com/missclick/seabattle/presentation/navigation/NavigationTree.kt
+++ app/src/main/java/com/missclick/seabattle/presentation/navigation/NavigationTree.kt
@@ -3,5 +3,6 @@ package com.missclick.seabattle.presentation.navigation
 sealed class NavigationTree(val route : String) {
 
     data object Menu : NavigationTree("menu")
+    data object Multiplayer : NavigationTree("multiplayer")
 
 }
